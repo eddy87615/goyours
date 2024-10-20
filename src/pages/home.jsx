fd9c7752dc0330850532b35a0bc00bac287909c0
Eddy Chen <eddychen615@gmail.com>
2024-10-20 22:01:20 +0900

    home contact us donr.

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,11 +1,11 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import { client } from '../cms/sanityClient';
 import { urlFor } from '../cms/sanityClient'; // 导入 urlFor
 import { FaLocationDot, FaArrowRightLong } from 'react-icons/fa6';
 import { LiaHandPointer } from 'react-icons/lia';
 
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Autoplay, Pagination, Navigation } from 'swiper/modules';
+import { Autoplay, Navigation } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/navigation';
 
@@ -195,11 +195,32 @@ const Wokringholiday = () => {
 };
 
 const Contactus = () => {
+  const ArcText = ({ text }) => {
+    return (
+      <div className="arc-text">
+        <svg width="800" height="800" viewBox="0 0 800 800">
+          <defs>
+            <path
+              id="circlePath"
+              d="
+                 M 160, 640
+                 A 320, 320, 0, 0, 1, 480, 320" // 使用320px (20rem)作為圓弧的半徑
+            />
+          </defs>
+          <text fill="#ff8c00" fontSize="20" fontWeight="bold">
+            <textPath href="#circlePath" startOffset="52%">
+              {text}
+            </textPath>
+          </text>
+        </svg>
+      </div>
+    );
+  };
   return (
     <>
       <div className="homeContactusDiv">
         <h2>Contact us</h2>
-        <div>
+        <div className="contactTxt01">
           <h3>聯絡GoYours</h3>
           <p>
             無論是短期進修、語言學校，還是打工度假體驗不同人生，
@@ -207,7 +228,7 @@ const Contactus = () => {
             背上背包，跟我們一起冒險，留下無悔的足跡！
           </p>
         </div>
-        <div>
+        <div className="contactTxt01">
           <h3>打工度假、留學免費諮詢</h3>
           <p>
             透過表單預約與我們一對一諮詢。
@@ -220,6 +241,7 @@ const Contactus = () => {
           href="./contact"
           alt="to Contact Us page"
         >
+          <ArcText text="填寫表單，我們將聯絡您！" />
           <FaArrowRightLong className="homecontactusArrow" />
         </a>
       </div>
